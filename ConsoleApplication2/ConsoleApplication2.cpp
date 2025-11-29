4851e22886223a6295695293e531a34da40421a5
1 <1@DESKTOP-69E422D>
2025-11-29 12:02:03 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int i = 300;
+	int j;
+	int sum;
+	cout << "Числа від 300 до 600, сума дільників яких кратна 10:\n";
+	do {
+		sum = 0;
+		j = 1;
+		do {
+			if (i % j == 0)
+				sum += j;
+			j++;
+		} while (j <= i / 2);
+		if (sum % 10 == 0)
+			cout << i << " ";
+		i++;
+	} while (i <= 600);
+	cout << "\n";
+	return 0;
+}
